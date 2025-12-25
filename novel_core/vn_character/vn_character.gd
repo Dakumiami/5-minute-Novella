62325c89e027deaeaac637e81098a06dafef193a
SimpleSyimyk <117256036+SimpleSyimyk@users.noreply.github.com>
2025-12-25 15:04:19 +0600

    ready to test
    just test

---
+++ novel_core/vn_character/vn_character.gd
@@ -0,0 +1,10 @@
+class_name VN_CharacterData
+extends Resource
+
+var character_name: String = ''
+var name_color: Color = Color(0, 0, 0)
+var sprite_frames: SpriteFrames = SpriteFrames.new()
+#var memory: Dictionary[NovelCharacter, int] = {}
+#var about_character: String = ''
+#var positon: int = 0
+#var sprite_flip: bool = true
---
+++ test/novel_core/vn_branch/vn_branch_builder_test.gd
@@ -0,0 +1,49 @@
+# GdUnit generated TestSuite
+class_name VnBranchBuilderTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source: String = 'res://novel_core/vn_branch/vn_branch_builder.gd'
+
+var VN_builder: VN_BranchBuilder = auto_free(VN_BranchBuilder.new())
+
+func test_question() -> void:
+	assert_not_yet_implemented()
+
+func question() -> void:
+	assert_not_yet_implemented()
+
+func update_memory() -> void:
+	assert_not_yet_implemented()
+
+func flip_sprite() -> void:
+	assert_not_yet_implemented()
+
+func animation() -> void:
+	assert_not_yet_implemented()
+
+func go_to_branch() -> void:
+	assert_not_yet_implemented()
+
+func narrative() -> void:
+	assert_not_yet_implemented()
+
+func speech() -> void:
+	assert_not_yet_implemented()
+
+func position() -> void:
+	assert_not_yet_implemented()
+
+func go_to_scene() -> void:
+	assert_not_yet_implemented()
+
+func condition() -> void:
+	assert_not_yet_implemented()
+
+func change_backgroung() -> void:
+	assert_not_yet_implemented()
+
+func _create() -> void:
+	assert_not_yet_implemented()
